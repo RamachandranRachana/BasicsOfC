f2ea9c8f1d5d4e69af334bd201c406467b5f5235
Rachana Ramachandran <rachanaramachandran24@gmail.com>
2021-09-21 22:34:41 -0400

    Introduction to Stacks

---
+++ STACKS/README.txt
@@ -0,0 +1,15 @@
+STACK DATA STRUCTURE
+LIFO - Last In First Out
+Stack of Plates
+
+Stack Operations
+1) Push   2) Pop  3) IsEmpty 3) Top Complexity of all operations O(1)
+
+
+Sections
+1)Implementation of Stack C++
+
+
+
+References
+1) https://www.geeksforgeeks.org/stack-data-structure/#design
---
+++ STACKS/Stack/Stack.cpp
@@ -0,0 +1,45 @@
+// Stack.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Stack
+{
+private:
+    std::vector<int> vect;
+
+public:
+    Stack()
+    {
+        vect.empty();
+    }
+    void Push(int val)
+    {
+        vect.push_back(val);
+    }
+    void Pop()
+    {
+        vect.pop_back();
+    }
+    void Top()
+    {
+        auto i = vect.size();
+        std::cout << vect[i - 1];
+    }
+    bool IsEmpty()
+    {
+        return vect.empty();
+    }
+};
+int main()
+{
+    auto stack = new Stack();
+    stack->Push(2);
+    stack->Push(4);
+    stack->Push(7);
+    stack->Top();
+    stack->Pop();
+    stack->Top();
+}
